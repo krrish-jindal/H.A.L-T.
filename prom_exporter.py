81f0fc0177d8399c2ecdc2115e671b7264de9aba
krrish-jindal <krrishjindal2002@gmail.com>
2023-02-04 19:37:38 +0530

    edit prom.py

--- prom_exporter.py
+++ prom_exporter.py
@@ -27,3 +27,29 @@ r = redis.Redis(
 host='localhost',
 port=6379,
 )
+class CustomCollector(object):     ## Class for CustomCollector which helps us to use different metric types
+    def collect(self):
+        
+        vden = get_vden_value() 
+        tlimit = get_tlimit_value()
+        eservice = get_eservice_value()
+        
+ 
+        value = CounterMetricFamily("HALT", 'Help text', labels='value')
+        value.add_metric(['vden'],vden)
+        value.add_metric(['tlimit'],tlimit)
+        value.add_metric(['eservice'],eservice)
+
+        yield value
+
+def run_http() :
+    with socketserver.TCPServer(("", PORT), Handler) as httpd:
+        print("serving at port", PORT)
+        httpd.serve_forever()
+
+if __name__ == '__main__':
+    start_http_server(8085)         ## port where metrics need to be exposed.
+    REGISTRY.register(CustomCollector())
+
+    while True:
+        time.sleep(0.01)               ## To collect the metrics for every 30s.
